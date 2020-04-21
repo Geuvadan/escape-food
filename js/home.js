0f71e203d11ff0ca03e2656670d43ba1e7b3be9b
geuv <geuvadan@gmail.com>
2020-04-21 14:50:36 +0200

    main de la home

--- js/home.js
+++ js/home.js
@@ -1,5 +1,16 @@
 const homeHTML = `
-    <h1>Ola q ase</h1>
+    <div class="home-container">
+      <section class="home-title">
+        <img src="./img/home-title.png" alt="game logo" />
+      </section>
+      <section class="home-name">
+        <img src="./img/home-subtitle.png" alt="En busca de la receta perfecta" />
+      </section>
+      <section class="home-call">
+        <h3><em>¿Serás capaz de resolverlo?</em></h3>
+        <button class="primary-btn">jugar</button>
+      </section>
+    </div>
   `;
 
 const printHome = () => {
