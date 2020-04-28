290a69fda117178b21c58d31b3f2b47212edf607
geuv <geuvadan@gmail.com>
2020-04-28 17:16:27 +0200

    texto home

--- js/home.js
+++ js/home.js
@@ -6,6 +6,9 @@ const homeHTML = `
       <section class="home-name">
         <img src="./img/home-subtitle.png" alt="En busca de la receta perfecta" />
       </section>
+      <section class="section-area">
+        <p>Pon a prueba tu ingenio y consigue llegar al final de este juego de lógica para llevarte un <span class="blue">SUCULENTO</span> premio</p>
+      </section>
       <section class="home-call">
         <h2 class="dark"><em>¿Serás capaz de resolverlo?</em></h2>
         <button id="play-btn" class="primary-btn play-btn">jugar</button>
