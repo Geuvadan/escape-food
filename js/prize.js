5dcb4febd8f966e9a7b05211b48f796ef839f670
geuv <geuvadan@gmail.com>
2020-04-28 14:31:04 +0200

    pagina de premios

--- js/prize.js
+++ js/prize.js
@@ -2,29 +2,53 @@ import storage from './storage.js';
 
 const priceHTML = (name) => `
     <div class="game-container">
-      <h3>TU PREMIO</h3>
+      <h3 class="prize-h3">¡FELICIDADES ${name.toUpperCase()}!</h3>
       <section class="section-area">
-        <p class="story-p">
-          Corres hacia el balcón y te reencuentras con tu humano, y con todos tus vecinos. 
-          Son las 20.00h y no quieres perder la ocasión de aplaudir, como cada día. Solo que 
-          hoy es diferente.
-        </p>
+        <h3>¡Tenemos DOS REGALOS para ti!</h3>
       </section>
 
-      <div class="section-img">
-        <img
-            srcset="./img/final/Balcon-600w.png 600w, 
-                  ./img/final/Balcon-1200w.png 1200w,
-                  ./img/final/Balcon-1800w.png 1800w"
-            src="./img/final/Balcon-12000w.png"
-            alt="a los balcones"
+      <div class="prize-frankie">
+        <div class="prize-frankie-img">
+          <img src="./img/premio/frankie-unboxing.gif" alt="unboxing Frankie"/>
+        </div>
+        <div class="prize-frankie-text">
+          <p><span class="blue">DOS SEMANAS</span> de Frankie desde <span class="blue">9,90€</span></p>
+          <p>+</p>
+          <p><span class="blue">CUPONAZO</span> descuento de <span class="blue">15€</span> con el código</p>
+          <p><span class="blue">"ESCAPEFOOD"</span></P>
+        </div>
+      </div>
+
+      <div class="navigation-prize">
+        <a href="https://frankietheking.com/wizard/" target="_blank" id="frankie-btn" class="primary-btn long-btn">¡Lo quiero!</a>
+      </div>
+
+      <div class="prize-pdf">
+        <div class="prize-frankie-text">
+          <p>Y descubre la cocina más perra con tu Recetario Frankie</p>
+          <p><span class="blue">GRATUITO</span></P>
+        </div>
+
+        <div class="navigation-prize">
+          <a href="../RecetarioFrankie.pdf" target="_blank" id="download-btn" class="primary-btn long-btn">DESCARGAR PDF</a>
+        </div>
+
+        <div class="prize-pdf-img">
+          <img
+            srcset="./img/premio/Hella_Centro-600w.png 600w, 
+                  ./img/premio/Hella_Centro-1200w.png 1200w,
+                  ./img/premio/Hella_Centro-1800w.png 1800w"
+            src="./img/premio/Hella_Centro-12000w.png"
+            alt="Hella cocinera"
           />
+        </div>
       </div>
 
       
+
+      
         
-        <button id="next-btn" class="primary-btn want-price-btn">Quiero mi premio!</button>
-      </section>
+        
     </div>
   `;
 
@@ -33,11 +57,6 @@ const printPrize = () => {
   const name = getItem('name');
   const mainContainer = document.getElementById('main');
   mainContainer.innerHTML = priceHTML(name);
-
-  const nextBtn = document.getElementById('next-btn');
-  nextBtn.addEventListener('click', () => {
-    page('/premio');
-  });
 };
 
 export default printPrize;
